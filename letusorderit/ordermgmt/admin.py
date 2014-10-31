660ff2598398f1905cc279556626772bd518d7ec
Andreas Madsack <andreas@madflex.de>
2014-10-31 23:18:06 +0100

    moar beauty in admin

--- letusorderit/ordermgmt/admin.py
+++ letusorderit/ordermgmt/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('master', 'description', 'shop_url', 'get_item_count')
+    search_fields = ('master', 'description')
+
+    def get_item_count(self, obj):
+        assert False
+        return obj.orderitem_set.count()
+    get_item_count.short_description = 'Item count'
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('get_order_description', 'user_email', 'item_identifier',
+                    'count')
+    search_fields = ('user_email', 'item_identifier')
+
+    def get_order_description(self, obj):
+        return obj.order.description
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
