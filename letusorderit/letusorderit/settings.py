3324142bc1c83fe1efbe17f4d5ad50a2b9a82fe5
Andreas Madsack <andreas@madflex.de>
2014-12-12 21:15:25 +0100

    enable pagination for all DRF views

--- letusorderit/letusorderit/settings.py
+++ letusorderit/letusorderit/settings.py
@@ -86,3 +86,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+############################################################
+
+REST_FRAMEWORK = {
+    'PAGINATE_BY': 10,                 # Default to 10
+}
